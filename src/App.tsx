30605abc04295459657008312777e2f08f475fd8
sabilawon <sabilawon@turmsadrain.com>
2023-04-24 13:25:30 +0100

    configurations done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App:React.FC = ():JSX.Element => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Biolerplate for React + Typescript + Tailwind 
+      </h1>
+    </div>
+  );
+}
+
+export default App;
